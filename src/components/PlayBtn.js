b059001f0527bdc63dbf5b7f39ab2d91e6bd1dca
Thi Nguyen <tttn.tn13@gmail.com>
2021-12-07 22:30:57 -0800

    change Play Button Color

--- src/components/PlayBtn.js
+++ src/components/PlayBtn.js
@@ -37,7 +37,7 @@ const PlayBtn = () => {
     <button
       type="button"
       id="startBtn"
-      className="btn btn-secondary"
+      className="btn btn-danger"
       onClick={() => {
         isPaused && handlePlayClick();
       }}
